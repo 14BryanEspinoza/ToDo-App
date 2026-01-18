d5084c99a9c10ce744cb4c33dd8448145135efa1
14bry <14bryanespinoza@gmail.com>
2026-01-18 06:47:21 -0600

    Feat: Estructura del header completa, Estilos completos en diseño mobile, tablet, desktop

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-	console.log("RUN");
-});
+// Constantes
+const dateID = document.getElementById('date')
+const dates = new Date()
+
+// Manejo de Fechas
+const option = {
+  weekday: 'long',
+  year: 'numeric',
+  day: 'numeric',
+}
+
+const personalizedDate = dates.toLocaleDateString('es-ES', option).toUpperCase()
+
+dateID.innerText = personalizedDate
